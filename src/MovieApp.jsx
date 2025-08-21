fbfdf995cfb48f464c76cf3d96edddf9d44aac82
saritadipa34 <saritadipa34@gmail.com>
2025-08-21 11:42:50 +0530

    UI created before fetching data

--- src/MovieApp.jsx
+++ src/MovieApp.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header"
+import MovieList from "./components/MovieList";
 const MovieApp=()=>{
     return(
         <div>
             <Header />
+            <MovieList />
         </div>
     )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,18 @@
+import Movie from "./Movie";
 
 const MovieList=()=>{
+
     return(
-        <div>
-            movielist
+        <div className="items-center justify-center px-[100px] py-10 flex flex-wrap gap-5">
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            <Movie />
+            
         </div>
     )
 }
