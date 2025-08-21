fca737ef280319c7d30785be7de1cd9536f91425
saritadipa34 <saritadipa34@gmail.com>
2025-08-21 12:45:46 +0530

    Add fetch logic for OMDB API

--- src/Context/MovieContext.jsx
+++ src/Context/MovieContext.jsx
@@ -1,8 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 
 export const MovieContext=createContext();
 
-export const MovieProvider=(children)=>{
+export const MovieProvider=({children})=>{
+
+    const getMovieData=async()=>{
+        try{
+        const apiKey=import.meta.env.VITE_API_KEY;
+        const response=await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=Inception`);
+        const data=await response.json();
+        console.log(data);
+    } catch (error){
+        console.log(error.message);
+    }
+    }
+
+    useEffect(()=>{
+        getMovieData();
+    },[]);
+
     return(
         <MovieContext.Provider value={{}} >
 {children}
